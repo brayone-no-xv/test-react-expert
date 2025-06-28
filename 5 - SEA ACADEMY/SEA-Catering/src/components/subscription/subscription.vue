f3b9f16b69a3960cae16aa36741f2b1ec4267eed
brayone-no-xv <hrepunzel@gmail.com>
2025-06-28 08:11:21 +0800

    💄 Design Subscription Page + subscription.vue

---
+++ 5 - SEA ACADEMY/SEA-Catering/src/components/subscription/subscription.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <section class="form-sea" id="subscription">
+    <div class="headline" style="background-color: blue">
+      <h1 class="headline-title">Daftar Akun SEA Catering</h1>
+      <br />
+      <p class="headline-subtitle">Mulai langganan makanan sehat anda</p>
+    </div>
+      <form action="" class="form-subscription">
+        <label for="nama">Nama Lengkap: </label>
+          <input type="text" />
+        <label for="email">Email: </label>
+          <input type="email" />
+        <label for="password">Password: </label>
+          <input type="password" />
+      </form>
+    <button class="footerline">Daftar Sekarang</button>
+  </section>
+</template>
+
+<style scoped></style>
