6b3c5c5c4fba8326d0216b7b44493c28d9f4d13d
brayone-no-xv <hrepunzel@gmail.com>
2025-06-28 08:12:27 +0800

    💄 Design Menu page + viewMenu.vue

---
+++ 5 - SEA ACADEMY/SEA-Catering/src/components/menu/viewMenu.vue
@@ -0,0 +1,15 @@
+<template>
+  <main id="menu">
+        <!-- hero -->
+        <heroSection />
+        <!-- carousel -->
+        <carousel />
+        <!-- testimonial -->
+  </main>
+</template>
+
+<style scoped></style>
+<script setup>
+  import heroSection from '../landingPage/hero/hero.vue';
+  import carousel from '../landingPage/carousel/carousel.vue';
+</script>
